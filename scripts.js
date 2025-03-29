d1b57fa52a2f084644f549441fad02a599c0e746
ILUZTAR ✦ <100333929+iluztar@users.noreply.github.com>
2025-03-29 13:42:29 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzgxACGQ5dCKWGVMSb9OYTwaxXfen954-ywk6YWsNzucK0gBw20yxl-ooOxRFtbiW3pfQ/exec'
-const form = document.forms['google-sheet']
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-    .catch(error => console.error('Error!', error.message))
-})
